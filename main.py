42760dce1f61298469f4b20997bdd47950983e9e
999-andreas <59205956+999-andreas@users.noreply.github.com>
2021-02-16 23:17:22 +0100

    ajout du comptage et du tri par couleur (théorique)

--- main.py
+++ main.py
@@ -8,13 +8,62 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
 
-
-# Create your objects here.
+# déclaration des variables et objets
 ev3 = EV3Brick()
+bouton=TouchSensor(Port.S1)
+capteur_couleur=ColorSensor(Port.S4)
+couleurs_legos=[]
+chrono=StopWatch
+
+# \\Programme//
+
+def comptage(couleurs_legos) : 
+# compte le nombre d'éléments passant par la machine et noter les couleur respective de chaque lego 
+# on pourrait se passer de la variable nb_lego mais elle permet de confirmer le nombre de legos à l'utilisateur
+    nb_lego=0
+    ev3.speaker.beep()
+    for comptage in range(8) :
+        print("montrer le lego")
+        wait(5000) 
+        if capteur_couleur.color() != None :
+            couleurs_legos.append(capteur_couleur.color())
+            nb_lego=comptage+1
+            print("couleur analysé")
+        else :
+            print("il y a : ",nb_lego,"legos")
+            ev3.speaker.beep()
+            return nb_lego
+               
+    print("il y a : 8 legos (c'est le maximum accepte)")
+    ev3.speaker.beep()
+    return nb_lego
+
+def case1() : 
+# les instructions moteur pour emener le legos à la case1
+def case2() : 
+# les instructions moteur pour emener le legos à la case2
+def case3() : 
+# les instructions moteur pour emener le legos à la case3
+
+def tri_par_couleur(comptage()) :
+# coeur du programme, envoie les legos dans la case corespondant à leur couleur 
+    case_rangement=[]
+    nb_legos = comptage()
+    for k in range(nb_legos-1) : 
+        if couleurs_legos[k] = RED : 
+            case1() 
+        elif couleurs_legos[k] = BLUE : 
+            case2() 
+        elif couleurs_legos[k] = GREEN : 
+            case3()
 
 
-# Write your program here.
-ev3.speaker.beep()
+
+def main() : 
+
+    if bouton.pressed()==True :
+        ev3.speaker.beep()
+        comptage() 
+
+main()
