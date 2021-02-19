19a2914909a1db2679187e96784ec2f0ff4641e0
999-andreas <59205956+999-andreas@users.noreply.github.com>
2021-02-20 00:11:53 +0100

    version opérationnelle
    le programme est opérationnel

--- main_v2.py
+++ main_v2.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -10,44 +10,54 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # déclaration des variables et objets
 ev3 = EV3Brick()
 bouton=TouchSensor(Port.S1)
-capteur_couleur=ColorSensor(Port.S3)
+capteur_couleur=ColorSensor(Port.S2)
 
 moteur_pince=Motor(Port.A)
-moteur_tapisroulant=Motor(Port.D)
+moteur_tapisroulant=Motor(Port.B)
 
 nb_legos=0
 
 
-
-
 # \\programme//
 
+print(capteur_couleur.color())
 
-# on s'assure que le truc soit bien à position d'origine 
-if bouton.pressed() == True : 
+# on s'assure que la machine soit bien à la position d'origine 
+if bouton.pressed() : 
+    moteur_tapisroulant.reset_angle(0)
 
-# la machine marchera tant que le capteur verra du [insérer la couleur qu'il détecte si il n'y pas de lego] càd lorsqu'il n'y a plus de lego dans la liste
-    while capteur_couleur.color() != None :
+    print("début du tri")
+# la machine marchera tant que le capteur verra pas du noir
+# càd lorsqu'il n'y a plus de lego dans la pile
+    while capteur_couleur.color() != Color.BLACK :
+        
 
-        print("début du tri")
 # chaque couleur de lego se voit assigner une 'case' les instructions moteur suivante envoie les legos dans leur case  
         if capteur_couleur.color() == Color.RED : 
-            moteur_tapisroulant.run_time(300,200)
-            moteur_pince.run_time(200,100) 
+            moteur_tapisroulant.run_target(500, 10)
+            
         elif capteur_couleur.color() == Color.BLUE :
-            moteur_tapisroulant.run_time(300,200)
-            moteur_pince.run_time(200,100) 
+            moteur_tapisroulant.run_target(500, 132)
+             
         elif capteur_couleur.color() == Color.GREEN : 
-            moteur_tapisroulant.run_time(300,200)
-            moteur_pince.run_time(200,100)
+            moteur_tapisroulant.run_target(500, 360)
+        
+        elif capteur_couleur.color() == Color.YELLOW : 
+            moteur_tapisroulant.run_target(500, 530)
+
+# on garde dans une variable le nombre de lego totale passé dans la machine   
         nb_legos+=1
 
-# consigne pour remettre la pince à sa place 
-        moteur_pince.run_until_stalled(120, duty_limit=50)
+# consigne pour lacher lego
+        moteur_pince.run_angle(1500,-180)
+        moteur_pince.run_angle(1500, 180)
+        
 
 # consigne pour que le moteur revienne a la position initiale càd lorsqu'il touche le bouton 
-        while bouton.pressed != True :
-            moteur_tapis_roulant.#roule
-
-    print("le tri est fini")
-    print(nb_lego,"legos sont passés dans la machine")
+        while not bouton.pressed() :
+            moteur_tapisroulant.run(-500)
+        moteur_tapisroulant.stop()
+        wait(500)
+        moteur_tapisroulant.reset_angle(0)
+        print("fin du tri")
+        print(nb_legos,"lego.s sont passés dans la machine")
