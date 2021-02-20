0e56b8aacc822a1509e84f9ef6047530078caea6
999-andreas <59205956+999-andreas@users.noreply.github.com>
2021-02-20 21:13:33 +0100

    Add files via upload

--- main_v2.py
+++ main_v2.py
@@ -9,13 +9,17 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # déclaration des variables et objets
 ev3 = EV3Brick()
-bouton=TouchSensor(Port.S1)
-capteur_couleur=ColorSensor(Port.S2)
+bouton = TouchSensor(Port.S1)
+capteur_couleur = ColorSensor(Port.S2)
 
-moteur_pince=Motor(Port.A)
-moteur_tapisroulant=Motor(Port.B)
+moteur_pince = Motor(Port.A)
+moteur_tapisroulant = Motor(Port.B)
 
-nb_legos=0
+lego_rouge = Color.RED
+lego_bleu = Color.BLUE
+lego_vert = Color.GREEN
+lego_jaune = Color.YELLOW
+couleur_legos = {"lego_rouge" : 0, "lego_bleu" : 0, "lego_vert" : 0, "lego_jaune" : 0, "total" : 0 }
 
 
 # \\programme//
@@ -33,20 +37,22 @@ if bouton.pressed() :
         
 
 # chaque couleur de lego se voit assigner une 'case' les instructions moteur suivante envoie les legos dans leur case  
-        if capteur_couleur.color() == Color.RED : 
+        if capteur_couleur.color() == lego_rouge : 
             moteur_tapisroulant.run_target(500, 10)
             
-        elif capteur_couleur.color() == Color.BLUE :
+        elif capteur_couleur.color() == lego_bleu :
             moteur_tapisroulant.run_target(500, 132)
              
-        elif capteur_couleur.color() == Color.GREEN : 
-            moteur_tapisroulant.run_target(500, 360)
+        elif capteur_couleur.color() == lego_vert : 
+            moteur_tapisroulant.run_target(500, 300)
         
-        elif capteur_couleur.color() == Color.YELLOW : 
-            moteur_tapisroulant.run_target(500, 530)
+        elif capteur_couleur.color() == lego_jaune : 
+            moteur_tapisroulant.run_target(500, 440)
 
-# on garde dans une variable le nombre de lego totale passé dans la machine   
-        nb_legos+=1
+# on garde dans un dictionnaire le nombre de lego totale passé dans la machine   
+        couleur_legos[capteur_couleur.color()]+=1
+        couleur_legos["total"]+=1
+        
 
 # consigne pour lacher lego
         moteur_pince.run_angle(1500,-180)
@@ -54,10 +60,11 @@ if bouton.pressed() :
         
 
 # consigne pour que le moteur revienne a la position initiale càd lorsqu'il touche le bouton 
-        while not bouton.pressed() :
-            moteur_tapisroulant.run(-500)
-        moteur_tapisroulant.stop()
-        wait(500)
-        moteur_tapisroulant.reset_angle(0)
-        print("fin du tri")
-        print(nb_legos,"lego.s sont passés dans la machine")
+    while not bouton.pressed() :
+        moteur_tapisroulant.run(-500)
+    moteur_tapisroulant.stop()
+    wait(500)
+    moteur_tapisroulant.reset_angle(0)
+    print("fin du tri")
+    print(couleur_legos)
+    print(couleur_legos["total"],"lego.s sont passés dans la machine")
